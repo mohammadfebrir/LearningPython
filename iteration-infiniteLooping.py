5d623defd67336a14fd0da3e50429baba200d72a
mohammadfebrir <mohammadfebrir@gmail.com>
2017-03-14 11:39:59 +0700

    learning iteration/infinite looping

---
+++ iteration-infiniteLooping.py
@@ -0,0 +1,13 @@
+def Main():
+	print("LEARNING PYTHON - ITERATION (infinite looping")
+	
+	num = 0
+	while num <= 0:
+		num = int(input("Please enter a positive integer: "))
+
+	while True:
+			print("hello world")	
+
+
+if __name__ == "__main__":
+	Main()
